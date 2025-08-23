818d0a29475dba9b70aec2babc70b0e7454e1919
Radilovk <radilov.k@gmail.com>
2025-08-23 04:27:53 +0300

    chore: remove refresh token flow

--- auth.js
+++ auth.js
@@ -5,23 +5,6 @@
     return localStorage.getItem(TOKEN_KEY);
   }
 
-  function setToken(token) {
-    localStorage.setItem(TOKEN_KEY, token);
-  }
-
-  async function refreshToken() {
-    const response = await fetch(`${API_BASE_URL}/api/refresh-token`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${getToken()}`
-      }
-    });
-    if (!response.ok) throw new Error('Refresh token failed');
-    const data = await response.json();
-    setToken(data.token);
-    return data.token;
-  }
-
   async function authorizedFetch(url, options = {}) {
     const headers = new Headers(options.headers || {});
     const token = getToken();
@@ -30,6 +13,5 @@
     return fetch(url, options);
   }
 
-  window.refreshToken = refreshToken;
   window.authorizedFetch = authorizedFetch;
 })();
